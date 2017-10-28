c29b2c57f1632a4589b9a9d11220ccb57d90cf3a
Farrez <shane.farrell@920Solutions.com>
2017-10-28 16:29:37 -0500

    Updated FTAs and add ignore null json values.

--- myretail-products-api/src/main/groovy/com/tgt/casestudy/myretailproductsapi/MyretailProductsApiApplication.groovy
+++ myretail-products-api/src/main/groovy/com/tgt/casestudy/myretailproductsapi/MyretailProductsApiApplication.groovy
@@ -2,7 +2,6 @@ package com.tgt.casestudy.myretailproductsapi
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean
 import org.springframework.web.client.RestTemplate
